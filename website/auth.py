9f36a347c3b82dce6a62a17a1a45eae4f995870e
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-15 16:44:16 +0300

    hashing password

--- website/auth.py
+++ website/auth.py
@@ -2,8 +2,11 @@ from flask import Blueprint, render_template,redirect, url_for ,request, flash
 from .import db
 from .models import User
 from flask_login import login_user, logout_user,login_required
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint("auth", __name__)
+
+
 
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
@@ -40,7 +43,7 @@ def sign_up():
     elif len(email) < 10:
         flash("Email is invalid.", category="error")
     else:
-        new_user = User(email=email, username=username, password1=password1)
+        new_user = User(email=email, username=username, password1=generate_password_hash(password1, method="sha256"))
         db.session.add(new_user)
         db.session.commit()
         flash("User created!")
