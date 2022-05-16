7fadd7ed418f0c329f786d3ec58e2718fd0a9e15
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-16 10:25:20 +0300

    settng up configuration

---
+++ config.py
@@ -0,0 +1,36 @@
+
+import os
+
+
+class Config:
+    BLOG_API_BASE_URL='http://quotes.stormconsultancy.co.uk/quotes.json'
+    BLOG_API_KEY='http://quotes.stormconsultancy.co.uk/quotes.json'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:isaac@localhost/blogs'
+   
+
+class ProdConfig(Config):
+    # SQLALCHEMY_DATABASE_URI ='postgresql://mxhwjrztvelttj:f78c638ad02dbae50ef11ca5e8acab88b2bdc1a3ae0e1fa274ff3186213aa009@ec2-107-22-238-112.compute-1.amazonaws.com:5432/de50v5u7j3bip6'
+    DEBUG=True
+
+class TestConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:james@localhost/test_user'
+    pass
+
+class DevConfig(Config):
+#    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:james@localhost/blogs'
+   DEBUG = True
+
+
+config_options={
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test': TestConfig
+}
