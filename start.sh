863c16fa228c04cff0f752f24d66eee9a5199722
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-18 00:38:26 +0300

    setting up email

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ export BLOG_API_KEY='http://quotes.stormconsultancy.co.uk/quotes.json'
 export SECRET_KEY='+x(9FbIvPtO}0~%lcFfJ'
 # export SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://moringa:james@localhost/blogs'
 export MAIL_USERNAME='makovejames@gmail.com'
-export MAIL_PASSWORD='0701937271'
+export MAIL_PASSWORD='jamo267@#'
 
 
 python3.9 manage.py server
