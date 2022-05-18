11f014b26cd548c428ffbd6384914f233e12f2c3
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-18 22:08:12 +0300

    modifying test

--- tests/user__test.py
+++ tests/user__test.py
@@ -11,3 +11,5 @@ class UserTest(unittest.TestCase):
     def test_no_access_password(self):
         with self.assertRaises(AttributeError):
             self.new_user.hashed_password
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('qwerty'))       
