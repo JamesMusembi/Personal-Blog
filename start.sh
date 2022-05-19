6ed8265f94ed180fcdd3bc7367bc20cf861f5349
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-19 20:48:35 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export BLOG_API_KEY='http://quotes.stormconsultancy.co.uk/quotes.json'
-export SECRET_KEY='+x(9FbIvPtO}0~%lcFfJ'
+ BLOG_API_KEY='http://quotes.stormconsultancy.co.uk/quotes.json'
+ SECRET_KEY='+x9FbIvPtO}0~%lcFfJ'
 # export SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://moringa:james@localhost/blogs'
 export MAIL_USERNAME='makovejames@gmail.com'
 export MAIL_PASSWORD='jamo267@#'
